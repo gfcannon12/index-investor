acf860e953c9f4021f050b691e6be59389f8ac2f
Gray Cannon <gfcannon12@users.noreply.github.com>
2019-01-16 00:10:14 -0500

    Added index_summary table

--- data_retrieval/make_chart.py
+++ data_retrieval/make_chart.py
@@ -19,13 +19,47 @@ def lambda_handler(event, context):
     
     df['dt'] = df['date'].apply(make_datetime)
     df.sort_values(by='dt', inplace=True)
-    most_recent_day = df['dt'].max()
-    mrd_str = df.loc[df['dt'] == most_recent_day, 'date'].values[0]
+
+    most_recent_day = df['dt'].max()    
+    day_ago = most_recent_day - datetime.timedelta(days=1)
+    month_ago = most_recent_day - datetime.timedelta(days=30)
     year_ago = most_recent_day - datetime.timedelta(days=365)
+    
+    def get_day(period_dt):
+        date_list = df.loc[df['dt'] == period_dt, 'date'].values
+        while len(date_list) == 0:
+            period_dt = period_dt - datetime.timedelta(days=1)
+            date_list = df.loc[df['dt'] == period_dt, 'date'].values
+        day_data = {}
+        day_data['date'] = df.loc[df['dt'] == period_dt, 'date'].values[0]
+        day_data[col] = df.loc[df['dt'] == period_dt, col].values[0]
+        return day_data
+    
+    mrd_str = get_day(most_recent_day)['date']
+    day_str = get_day(day_ago)['date']
+    month_str = get_day(month_ago)['date']
+    year_str = get_day(year_ago)['date']
+    
+    mrd_value = get_day(most_recent_day)[col]
+    day_value = get_day(day_ago)[col]
+    month_value = get_day(month_ago)[col]
+    year_value = get_day(year_ago)[col]
+    
     max_value = df[col].max()
     max_df = df[df[col] == max_value]
+    max_df = max_df.loc[max_df['dt'] == max_df['dt'].min(), :]
     max_dt = max_df['dt'].values[0]
     max_dt_dt = datetime.datetime.utcfromtimestamp(max_dt.tolist()/1e9)
+    max_str = max_df['date'].values[0]
+    
+    summary_df = pd.DataFrame({
+            'period': ['today', 'yesterday', 'month', 'year', 'max'],
+            'date': [mrd_str, day_str, month_str, year_str, max_str],
+            'value': [mrd_value, day_value, month_value, year_value, max_value],
+            'metric': [col, col, col, col, col]
+    })
+    
+    summary_df.to_sql('index_summary', conn, if_exists='replace', index=False)
     
     if max_dt_dt > year_ago:
         chart_start = year_ago
